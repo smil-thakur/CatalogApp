78b13c88960bf68bc450eaf8c2186ee533e083fe
smil <smil.raj.thakur@gmail.com>
2021-08-14 16:39:26 +0530

    day-4 done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,10 +7,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("login page",
+      child: Column(
+        children: [
+          Image(image: AssetImage("assets/images/login_image.png")),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Welcome",
             style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue)),
+                fontSize: 22,
+                fontFamily: GoogleFonts.lato().fontFamily,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter UserName", labelText: "UserName"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
