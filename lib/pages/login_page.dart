990f807786da902216fa101a200992b1135e442f
smil <smil.raj.thakur@gmail.com>
2021-08-15 21:10:24 +0530

    day-6 added animated button(container)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,16 @@ import 'package:catalog_app/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool pressed = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,7 +23,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                   fontSize: 22,
                   fontFamily: GoogleFonts.lato().fontFamily,
@@ -32,6 +39,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                         hintText: "Enter UserName", labelText: "UserName"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -43,15 +54,40 @@ class LoginPage extends StatelessWidget {
                 ],
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  pressed = true;
+                });
+                await Future.delayed(Duration(seconds: 1));
                 Navigator.pushNamed(context, MyRoutes.homeRoute);
               },
-              child: Text("login"),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
+              child: AnimatedContainer(
+                height: 50,
+                width: pressed ? 50 : 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(pressed ? 50 : 10),
+                  color: Colors.blue,
+                ),
+                child: Center(
+                    child: pressed
+                        ? Icon(Icons.done)
+                        : Text(
+                            "login",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                duration: Duration(seconds: 1),
               ),
-            ),
+            )
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+            //   },
+            //   child: Text("login"),
+            //   style: TextButton.styleFrom(
+            //     primary: Colors.white,
+            //   ),
+            // ),
           ],
         ),
       ),
